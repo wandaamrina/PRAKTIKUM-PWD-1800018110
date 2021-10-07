007168ccfe08513d6cbfcfc5fdbc5ddd73dabc2a
Wanda Amrina Rosyada <52875758+wandaamrina@users.noreply.github.com>
2021-10-07 22:29:26 +0700

    Pertemuan 1

---
+++ upload_file/download.php
@@ -0,0 +1,8 @@
+<?php
+$myDir = "C:/xampp/htdocs/pwd-1800018121/upload_file/";
+$dir = opendir($myDir);
+echo "Isi folder (klik link untuk download : <br>";
+while($tmp = readdir($dir)){
+	echo "<a href='$tmp' target='_blank'>$tmp</a><br>";
+}
+closedir($dir);
---
+++ upload_file/latihan1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+</head>
+
+<body>
+    <form enctype="multipart/form-data" method="post" action="upload.php">
+        File yang diupload: <input type="file" name="fupload"><br>
+        Deskripsi File : <br><textarea name="deskripsi" rows="6" cols="20"></textarea><br>
+        <input type=submit value=Upload>
+    </form>
+    <br>
+    <a href="download.php">Klik untuk download</a>
+</body>
+
+</html>
